4f36e07a6026f6377b26be856fb0f5e8721ed966
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-03 17:27:02 +0100

    implement data in different file

---
+++ data.php
@@ -0,0 +1,28 @@
+<?php 
+
+$movieList = [
+    [
+        "movie_element" => "la_teoria_del_tutto",
+        "movie_info" => [
+            "name" => "La Teoria del Tutto",
+            "cast" => ['Eddie Readmayne', 'Felicity Jones'],
+            "length" => "140",
+        ]
+    ],
+    [
+        "movie_element" => "pretty_woman",
+        "movie_info" => [
+            "name" => "Pretty Woman",
+            "cast" => ['Julia Roberts', 'Richard Gere', 'Laura San Giacomo'],
+            "length" => "'romance','romantic comedy','comedy'",
+        ]
+    ],
+    [
+        "movie_element" => "interstellar",
+        "movie_info" => [
+            "name" => "Interstellar",
+            "cast" => ['Matthew McConaughey','Jessica Chastain','Anne Hathaway','Mackenzie Foy','Timothée Chalamet','Matt Damon'],
+            "length" => "169",
+        ]
+    ],
+];
