5587dbe4728792c23d1a387d81163a89b316f753
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-03 14:17:41 +0100

    create Movie class

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php-oop-1</title>
+</head>
+<body>
+    
+
+<?php
+    class Movie{
+        public $name;
+        public $cast;
+        public $length;
+    }
+
+    
+
+
+?>
+
+
+</body>
+</html>
